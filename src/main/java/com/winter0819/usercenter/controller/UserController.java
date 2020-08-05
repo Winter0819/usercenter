29cff2f545ce34dc9668c381be8a119fc9c9bac5
wangjin <wangj@dachentech.com.cn>
2020-08-05 19:36:37 +0800

    111

--- src/main/java/com/winter0819/usercenter/controller/UserController.java
+++ src/main/java/com/winter0819/usercenter/controller/UserController.java
@@ -1,6 +1,8 @@
 package com.winter0819.usercenter.controller;
 
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.ResponseEntity;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -12,8 +14,11 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/user")
 public class UserController {
 
+    private final static Logger logger = LoggerFactory.getLogger(UserController.class);
+
     @RequestMapping(value = "/getUserById" , method = RequestMethod.GET)
     public String getUserById(@RequestParam String userid) {
+        logger.info("{},{},username-{}","UserController","getUserById",userid);
         return "winter0819";
     }
 }
